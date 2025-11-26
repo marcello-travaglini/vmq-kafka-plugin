9d03717df4f712d15691ebc63129aab9c911939f
Marcello Travaglini <marcello.travaglini@gmail.com>
2025-11-26 12:05:58 +0100

    Add support for MQTT 5

--- src/vmq_kafka_plugin.erl
+++ src/vmq_kafka_plugin.erl
@@ -26,6 +26,9 @@
 %% Export the on_publish/6 callback so VerneMQ can call it
 -export([on_publish/6]).
 
+%% Export the on_publish/6 callback so VerneMQ can call it
+-export([on_publish_m5/7]).
+
 %%--------------------------------------------------------------------
 %% on_publish/6
 %%
@@ -40,7 +43,7 @@
 %% @param _IsRetain   - Boolean flag indicating if this is a retained message
 %%
 %% Behaviour:
-%%   - This hook is called synchronously by VerneMQ when a message is published.
+%%   - This hook is called synchronously by VerneMQ when a message is published with MQTT 2/3 protocol.
 %%   - We immediately enqueue the message into our dispatcher process for
 %%     asynchronous handling (e.g., mapping to Kafka topics and producing).
 %%   - We do not perform any blocking operations here to avoid slowing down
@@ -49,6 +52,39 @@
 %%     to subscribers and other hooks.
 %%--------------------------------------------------------------------
 on_publish(_UserName, {MountPoint, ClientId}, _QoS, Topic, Payload, _IsRetain) ->
+    %% Forward the publish event to the dispatcher.
+    %% The dispatcher will decide how to handle the message (e.g., match
+    %% against configured mappings, send to Kafka via worker processes).
+    %% We pass MountPoint, ClientId, Topic, Payload, QoS and _IsRetain (even if unused here).
+    vmq_kafka_plugin_dispatcher:enqueue(MountPoint, ClientId, Topic, Payload, _QoS, _IsRetain),
+
+    %% Always return 'ok' so VerneMQ continues processing the publish normally.
+    ok.
+
+%%--------------------------------------------------------------------
+%% on_publish/6
+%%
+%% @param _UserName   - Username of the publishing client (ignored here)
+%% @param {MountPoint, ClientId} - Tuple containing:
+%%        MountPoint: binary identifying the mountpoint (namespace) of the client
+%%        ClientId:   binary identifying the MQTT client ID
+%% @param _QoS        - Quality of Service level of the publish (0, 1, or 2)
+%%                      Ignored here, but passed along to the dispatcher.
+%% @param Topic       - MQTT topic (binary or list of segments)
+%% @param Payload     - Message payload (binary or iolist)
+%% @param _IsRetain   - Boolean flag indicating if this is a retained message
+%% @param _Properties - Contains specific propertie for MQTT 5 protocol
+%%
+%% Behaviour:
+%%   - This hook is called synchronously by VerneMQ when a message is published with MQTT 5 protocol.
+%%   - We immediately enqueue the message into our dispatcher process for
+%%     asynchronous handling (e.g., mapping to Kafka topics and producing).
+%%   - We do not perform any blocking operations here to avoid slowing down
+%%     the broker's publish path.
+%%   - Always return 'ok' to let VerneMQ continue delivering the message
+%%     to subscribers and other hooks.
+%%--------------------------------------------------------------------
+on_publish_m5(_UserName, {MountPoint, ClientId}, _QoS, Topic, Payload, _IsRetain, _Properties) ->
     %% Forward the publish event to the dispatcher.
     %% The dispatcher will decide how to handle the message (e.g., match
     %% against configured mappings, send to Kafka via worker processes).
